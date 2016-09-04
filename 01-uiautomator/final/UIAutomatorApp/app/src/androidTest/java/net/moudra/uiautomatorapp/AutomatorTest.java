68b77e22531d699106d5470e449a956ed1581a27
Jana Moudrá <jana.moudra@gmail.com>
2016-09-05 00:45:48 +0200

    UI Automator demo

---
+++ 01-uiautomator/final/UIAutomatorApp/app/src/androidTest/java/net/moudra/uiautomatorapp/AutomatorTest.java
@@ -0,0 +1,140 @@
+package moudra.net.uiautomatorapp;
+
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class AutomatorTest {
+    private UiDevice uiDevice;
+
+    @Before
+    public void beforeTest() {
+        uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        uiDevice.pressHome();
+    }
+
+    @After
+    public void afterTest() {
+        uiDevice.pressHome();
+    }
+
+    @Test
+    public void testCalculator() throws Exception {
+        // Home screen apps button
+        UiObject appButton = uiDevice.findObject(new UiSelector().descriptionContains("Apps"));
+        Assert.assertTrue(appButton.exists());
+
+        appButton.clickAndWaitForNewWindow();
+
+        // Scrollable view with apps
+        UiScrollable appViews = new UiScrollable(new UiSelector().scrollable(true));
+        Assert.assertTrue(appViews.exists());
+
+        appViews.setAsHorizontalList();
+
+        // Find calculator application
+        UiObject calculatorApp = appViews.getChildByText(new UiSelector()
+                .className("android.widget.TextView"), "Calculator");
+        Assert.assertTrue(calculatorApp.exists());
+
+        calculatorApp.clickAndWaitForNewWindow();
+
+        // Use calculator app
+        UiObject clearButton = uiDevice.findObject(new UiSelector().textMatches("clr|del"));
+        Assert.assertTrue(clearButton.exists());
+        clearButton.click();
+
+        UiObject threeButton = uiDevice.findObject(new UiSelector().text("3"));
+        Assert.assertTrue(threeButton.exists());
+        threeButton.click();
+
+        UiObject plusButton = uiDevice.findObject(new UiSelector().text("+"));
+        Assert.assertTrue(plusButton.exists());
+        plusButton.click();
+
+        UiObject fiveButton = uiDevice.findObject(new UiSelector().text("5"));
+        Assert.assertTrue(fiveButton.exists());
+        fiveButton.click();
+
+        UiObject equalsButton = uiDevice.findObject(new UiSelector().text("="));
+        Assert.assertTrue(equalsButton.exists());
+        equalsButton.click();
+
+        UiObject display = uiDevice.findObject(new UiSelector()
+                .resourceId("com.android.calculator2:id/display"));
+        Assert.assertTrue(display.exists());
+
+        // Validate
+        UiObject displayNumber = display.getChild(new UiSelector().index(0));
+        Assert.assertTrue(displayNumber.exists());
+        Assert.assertEquals(displayNumber.getText(), "8");
+    }
+
+    @Test
+    public void testBrowserApp() throws Exception {
+        // Home screen apps button
+        UiObject appButton = uiDevice.findObject(new UiSelector().descriptionContains("Apps"));
+        Assert.assertTrue(appButton.exists());
+
+        appButton.clickAndWaitForNewWindow();
+
+        // Scrollable view with apps
+        UiScrollable appViews = new UiScrollable(new UiSelector().scrollable(true));
+        Assert.assertTrue(appViews.exists());
+
+        appViews.setAsHorizontalList();
+
+        // Find browser application
+        UiObject browserApp = appViews.getChildByText(new UiSelector()
+                .className("android.widget.TextView"), "Browser");
+        Assert.assertTrue(browserApp.exists());
+        browserApp.clickAndWaitForNewWindow();
+
+        // Browser App set url
+        UiObject urlForm = uiDevice.findObject(new UiSelector()
+                .resourceId("com.android.browser:id/url"));
+        Assert.assertTrue(urlForm.exists());
+        urlForm.click();
+        urlForm.setText("www.google.com");
+        uiDevice.pressEnter();
+
+        // Wait to load page
+        SystemClock.sleep(10000);
+
+        // Show menu
+        uiDevice.pressMenu();
+
+        // Find text on page
+        UiObject findButton = uiDevice.findObject(new UiSelector()
+                .text("Find on page"));
+        Assert.assertTrue(findButton.exists());
+        findButton.click();
+
+        UiObject findView = uiDevice.findObject(new UiSelector()
+                .resourceId("android:id/edit"));
+        Assert.assertTrue(findView.exists());
+        findView.setText("Google");
+        uiDevice.pressEnter();
+
+        SystemClock.sleep(2000);
+
+        // Dismiss search
+        UiObject okButtonView = uiDevice.findObject(new UiSelector()
+                .resourceId("com.android.browser:id/iconcombo"));
+        Assert.assertTrue(okButtonView.exists());
+        okButtonView.click();
+    }
+}
