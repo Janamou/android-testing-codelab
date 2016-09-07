bc1e8d208adf3fe030895dc65bd3f455af35795b
Jana Moudrá <jana.moudra@gmail.com>
2016-09-07 11:22:49 +0200

    Test recorder

---
+++ 03-test-recorder/README.md
@@ -0,0 +1,42 @@
+# Part 3 - Espresso Test Recorder
+
+Espresso Test Recorder is a new feature from the Android Studio 2.2 Preview 3. 
+
+This feature makes easy creating UI tests by recording interaction with the application. Espresso tests are then generated so there is no need for programming work.
+
+## Code lab
+
+### Step 1 - No test
+
+We start with the folder `step_0_start`.
+
+### Step 2 - Start recording
+
+Select `Run/Record Espresso Test`.
+
+### Step 3 - Device
+
+Select the device from Device chooser.
+
+### Step 4 - Use your App
+
+Use your app. You can see that your activity is tracked in the new Events window.
+
+### Step 5 - Assertions
+
+You can also add assertions to check if app works as expected. Create assertion with Add assertion button and then select the element which you want to check on the screen. And see the assertion you want to check the element against.
+
+### Step 6 - Complete recording
+
+Complete recording when you are finished.
+
+### Step 7 - Test class
+
+Give your test class some name.
+
+### Step 8 - Run the test
+
+Run the test and see how it works.
+
+# More resources
+* [Espresso Test Recorder](http://tools.android.com/tech-docs/test-recorder)
---
+++ 03-test-recorder/final/TestRecorderApp/app/src/androidTest/java/net/moudra/testrecorderapp/TestRecorderTest.java
@@ -0,0 +1,94 @@
+package net.moudra.testrecorderapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TestRecorderTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testRecorderTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.message_btn), withText("Send"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        pressBack();
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.message_edittext),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("Helooooo"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.message_btn), withText("Send"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.message_textview), withText("Helooooo"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_detail),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Helooooo")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
