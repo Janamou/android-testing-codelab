b94c92a8652dfdf7acdc897123731cbded44ecf3
Jana Moudrá <jana.moudra@gmail.com>
2016-09-08 17:18:45 +0300

    Espresso starter app

---
+++ 02-espresso/start/EspressoApp/app/src/androidTest/java/net/moudra/espressoapp/EspressoTest.java
@@ -0,0 +1,22 @@
+package net.moudra.espressoapp;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+    
+}
