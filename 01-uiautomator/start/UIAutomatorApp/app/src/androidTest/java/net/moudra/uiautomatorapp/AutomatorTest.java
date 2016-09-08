bfbcc643a99614d3fde34cacc050d186536b2d9a
Jana Moudrá <jana.moudra@gmail.com>
2016-09-08 17:13:35 +0300

    Automator start project

---
+++ 01-uiautomator/start/UIAutomatorApp/app/src/androidTest/java/net/moudra/uiautomatorapp/AutomatorTest.java
@@ -0,0 +1,24 @@
+package net.moudra.uiautomatorapp;
+
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class AutomatorTest {
+
+}
